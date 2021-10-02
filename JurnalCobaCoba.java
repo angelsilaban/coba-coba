7ce537cd77a0195c2856c1f391785c9dd66be8ab
angel silaban <magdalenagela20@gmail.com>
2021-10-02 10:43:23 +0700

    first commit

---
+++ JurnalCobaCoba.java
@@ -0,0 +1,9 @@
+public class JurnalCobaCoba {
+	public static void main(String[] args) {
+		System.out.println("Haii,namaku Angel Silaban");
+		System.out.println("dgn NIM 6706210052");
+		System.out.println("saya menyukai makanan pedas dan coklat");
+		System.out.println("Hobi saya nonton drakor,makan, dance, dll");
+	}
+}
+	
